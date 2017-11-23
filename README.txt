33b353f9afb565f24036157c68c955d6909771cc
David Lee <dlee@nexstra.com>
2017-11-22 23:58:50 -0500

    Docker-repo-yum

---
+++ README.txt
@@ -0,0 +1,1 @@
+https://github.com/synfinatic/docker-yum-repo
--- startup.sh
+++ startup.sh
@@ -5,5 +5,11 @@ mkdir /logs/nginx
 mkdir /logs/supervisord
 
 chown nginx:nginx /logs/nginx
+for d in $(find /repo -type d  ) ; do
+    if  ls $d/*.rpm >/dev/null ; then 
+        createrepo  --update $d
+    fi
+done
+    
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
