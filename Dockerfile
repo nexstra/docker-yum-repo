7607a4e09c4c5b95c6303a1c0458a6479f34d52c
David Lee <dlee@nexstra.com>
2017-12-17 09:19:50 -0500

    docker-yum-repo

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,14 @@
-FROM centos:7
-MAINTAINER Diego Gutierrez <dgutierrez1287@gmail.com>
-
-RUN yum -y install epel-release
-RUN yum -y update
-RUN yum -y install ruby gcc ruby-devel supervisor createrepo yum-utils nginx
-RUN yum clean all
-RUN gem install rb-inotify
+FROM amazonlinux
+MAINTAINER David Lee <dlee@nexstra.com>
+RUN yum clean all && yum -y install epel-release && \
+    yum-config-manager --enable epel && \
+    yum -y update
+RUN yum -y install createrepo inotify-tools findutils util-linux
 
 RUN mkdir /repo
-RUN mkdir /logs
-
-ADD nginx.conf /etc/nginx/nginx.conf
-ADD supervisord.conf /etc/supervisord.conf
-ADD scan_repo.rb /
 ADD startup.sh /
-
-RUN chmod 755 /scan_repo.rb
 RUN chmod 755 /startup.sh
-
-EXPOSE 80
-VOLUME /repo /logs
+VOLUME /repo
 
 ENTRYPOINT ["/startup.sh"]
 
--- startup.sh
+++ startup.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+cd "${1:-/repo}"
 
-mkdir /logs/repo-scanner
-mkdir /logs/nginx
-mkdir /logs/supervisord
+runcr() {
+  local dir="$1"
+  while  [ -f "${dir}.new" ] ; do
+     rm -f "${dir}.new" ;
+    echo running createrepo $dir
+    flock "$dir" createrepo --update "$dir"
+  done
+}
+watched=()
 
-chown nginx:nginx /logs/nginx
-for d in $(find /repo -type d  ) ; do
-    if  ls $d/*.rpm >/dev/null ; then 
-        createrepo  --update $d
+  for d in * ; do
+    if [ -d "$d" -a "$d" != "repodata" ]  ; then
+      if [ $(find "$d" -type f -name \*.rpm | wc -l) -gt 0 ] ; then 
+        echo "Initializing $d"
+        createrepo "$d"
+      fi
+      watched+=("$d")
     fi
-done
-    
+  done
 
-exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+echo "Watching ${watched[*]}"
+inotifywait -m --exclude 'repodata.*' \
+   -e close_write -e move -e delete  \
+   "${watched[@]}"  | 
+while read watched events file ; do
+   printf "Watched: %s %s %s\n" $watched $events $file 
+   if [[ "$file" =~ .*\.rpm ]] ; then  
+     dir="${watched%%/}"
+     touch "${dir}.new"
+     if flock -n "${dir}" true ; then 
+        echo "running cr"
+        runcr "${dir}" &
+      fi 
+   fi
+done
+   
